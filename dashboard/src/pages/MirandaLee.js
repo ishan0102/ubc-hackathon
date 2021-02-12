e477c0f6d8705825f21674cc60742acb34a9d617
ishan0102 <ishan0102@gmail.com>
2021-02-12 17:54:40 -0600

    Add avatars and clean up page layout

--- dashboard/src/pages/MirandaLee.js
+++ dashboard/src/pages/MirandaLee.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
 import {
   Typography,
-  Button
+  Button,
+  Avatar
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Graphs from '../graphs/Graphs';
+import mirandalee from '../assets/mirandalee.jpg'
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    marginLeft: '1em',
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
 
 export default function MirandaLee(props) {
+  const classes = useStyles();
+  
   return( 
     <div>
-      <Typography variant='h2'>Miranda Lee</Typography>
+      <div style={{'justifyContent': 'center', 'alignItems': 'center', 'display': 'flex', 'flexDirection': 'row'}}>
+        <Typography variant='h2'>Miranda Lee</Typography>
+        <Avatar alt="Miranda Lee" src={mirandalee} className={classes.large} />
+      </div>
       <Graphs notifications={props.notifications} />
     </div>
   )
